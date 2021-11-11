fc94fd23d4a04b56b276627bec382005024b4869
Elena <freshjs.ftm@gmail.com>
2021-11-11 11:03:03 +0200

    Add App.js without JSX

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const reactElement = 
+<React.StrictMode>
+  <App />
+  {/* React.createElement(App); */}
+</React.StrictMode>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement,
   document.getElementById('root')
 );
 
@@ -15,3 +19,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
