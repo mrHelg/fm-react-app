ff2f402d32871f0fe397cf256236044f766a8b15
Elena <freshjs.ftm@gmail.com>
2021-11-12 10:26:29 +0200

    Add state for App

--- src/App.js
+++ src/App.js
@@ -6,15 +6,39 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      fname:'Elon',
-      sname:'Musk'
+      users:[
+        {
+          id:1,
+          fname:'Elon'
+        },
+        {
+          id:2,
+          fname:'Elen'
+        },
+        {
+          id:3,
+          fname:'Tim'
+        },
+        {
+          id:4,
+          fname:'Bob'
+        },
+        {
+          id:5,
+          fname:'Sasha'
+        },
+        {
+          id:6,
+          fname:'Rob'
+        },
+      ]
     }
   }
   render(){
     const {fname} = this.state;
     return <>
     <Ciao name={fname} />
-    <Ciao name='Elen' />
+
     </>;
   }
 }
