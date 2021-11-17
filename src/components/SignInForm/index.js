5bbd4158add0a8e0e49ec1e1328e44c3844bc85b
Elena <freshjs.ftm@gmail.com>
2021-11-17 11:32:50 +0200

    Add validation

--- src/components/SignInForm/index.js
+++ src/components/SignInForm/index.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import styles from './SignInForm.module.css';
 
+const initialValues = {
+      email:'',
+      pwd:'',
+      emailValid:false,
+      pwdValid:false
+    };
+
 class SignInForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {...initialValues}
+  }
+
+  handleInput = ({target:{name, value}}) => this.setState({
+    [name]:value,
+    [`${name}Valid`]: !value.includes(' ')
+  })
+
+  handleForm = (event)=>{
+    event.preventDefault();
+    //event.target.reset();
+    this.setState({...initialValues})
+  }
   render() {
+    const {email, pwd, emailValid, pwdValid} = this.state;
+    const emailClassName = `${styles.input} ${emailValid?styles.valid : styles.invalid}`;
+    const pwdClassName = `${styles.input} ${pwdValid?styles.valid : styles.invalid}`;
+
     return (
-      <form className={styles.container}>
-        <input type='email' 
+      <form className={styles.container}
+      onSubmit={this.handleForm}>
+        <input type='text' 
         name='email'  
-        className={styles.input}/>
+        value={email}
+        placeholder='email'
+        onChange={this.handleInput}
+        className={emailClassName}/>
         <input type='password' 
         name='pwd'  
-        className={styles.input}/>
+        value={pwd}
+        placeholder='password'
+        onChange={this.handleInput}
+        className={pwdClassName}/>
         <input type='submit' 
         value='Sign In' 
         className={styles.btn}/>
