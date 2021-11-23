6f221085027938fe6736535f780168202dbf7e36
Олег <osumsky@gmail.com>
2021-11-23 10:59:48 +0200

    add Query to random users

--- src/components/UsersLoader/index.js
+++ src/components/UsersLoader/index.js
@@ -3,11 +3,45 @@ import React, { Component } from 'react';
 class UsersLoader extends Component {
   constructor(props) {
     super(props);
-    
+    this.state = {
+      isError: false,
+      isLoaded: false,
+      users: [],
+    };
   }
-  
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=10&page=2&seed=users')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ isLoaded: true, users: data.results });
+      })
+      .catch((error) => {
+        this.setState({ isError: true });
+      });
+  }
+
   render() {
-    return <div></div>;
+    const { users, isLoaded, isError } = this.state;
+
+    if (isError) {
+      return <div>Error</div>;
+    }
+    if (isLoaded) {
+      return (
+        <div>
+          <h2>User list</h2>
+          <ul>
+            {users.map((user) => (
+              <li key={user.login.uuid}>{JSON.stringify(user, null, 7)}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
